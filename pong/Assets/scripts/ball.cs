52574d88236bd832d76b36f8fa76d0e49fdc343e
Charlie <charliecuddy4@gmail.com>
2018-04-15 23:29:48 -0500

    mess around with if statement for initial direction of the ball

--- pong/Assets/scripts/ball.cs
+++ pong/Assets/scripts/ball.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class ball : MonoBehaviour {
     public float speed = 10f;
     private Rigidbody2D rb;
+
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.right * speed;
+        rb.velocity = Vector2.left * speed;
+//       if (this.randomNum == 0) {
+//           rb.velocity = Vector2.right * speed;
+//       } else {
+//           rb.velocity = Vector2.left * speed;
+//       }
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
