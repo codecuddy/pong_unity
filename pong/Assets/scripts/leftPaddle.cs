4f5e237662af1a450483a0a91f483459f593790a
Charlie <charliecuddy4@gmail.com>
2018-04-15 22:05:38 -0500

    move paddles, bounce ball straight ahead

---
+++ pong/Assets/scripts/leftPaddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftPaddle : MonoBehaviour {
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w") && transform.position.y < 3.75) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s") && transform.position.y > -3.75) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a") && transform.position.x > -6) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("d") && transform.position.x < -1) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+	}
+}
---
+++ pong/Assets/scripts/rightPaddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightPaddle : MonoBehaviour {
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up") && transform.position.y < 3.75) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("down") && transform.position.y > -3.75) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("left") && transform.position.x > 1) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("right") && transform.position.x < 6) {
+            //Time.deltaTime is computer time in seconds so if there is lag it stay consistent
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+	}
+}
