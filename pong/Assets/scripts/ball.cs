e21d0482b9d6f50853b90b43dc0b1c6e595834d2
Charlie <charliecuddy4@gmail.com>
2018-04-18 20:24:38 -0500

    add background, and foundation for change of direction based on location on paddle that the ball hits

--- pong/Assets/scripts/ball.cs
+++ pong/Assets/scripts/ball.cs
@@ -1,27 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ball : MonoBehaviour {
     public float speed = 10f;
     private Rigidbody2D rb;
-    private float randomNumber;
+    private int direction = -1;
+    public int leftScore = 0;
+    public int rightScore = 0;
+    //public Text leftText = 0;
+    //public Text rightText = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
-        randomNumber = Random.Range(1, 3);
-          if (randomNumber == 1) {
-            Debug.Log(randomNumber);
-             rb.velocity = Vector2.right * speed;
-          } if (randomNumber == 2) {
-            Debug.Log(randomNumber);
-             rb.velocity = Vector2.left * speed;
-          }
-	 }
+        StartBall();
+    }
+
+    void StartBall() {
+        rb.velocity = new Vector2(direction, 0) * speed; 
+    }
 
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    void OnCollisionEnter2D (Collision2D obj) {
+        if (obj.gameObject.tag == "rightPaddle") {
+            float y = (transform.position.y - obj.transform.position.y) / obj.collider.bounds.size.y;
+            Vector2 dir = new Vector2(-1, y).normalized;
+            rb.velocity = dir * speed;
+                
+        }
+
+        if (obj.gameObject.tag == "leftPaddle")
+        {
+            float y = (transform.position.y - obj.transform.position.y) / obj.collider.bounds.size.y;
+            Vector2 dir = new Vector2(1, y).normalized;
+            rb.velocity = dir * speed;
+        }
+
+        if (obj.gameObject.tag == "rightWall") {
+            transform.position = new Vector3(0, 0, 0);
+            rb.velocity = Vector2.zero;
+            direction = -1;
+            Invoke("StartBall", 1);
+            //leftScore++;
+            //leftText.text = leftScore.ToString();
+        }
+
+        if (obj.gameObject.tag == "leftWall")
+        {
+            transform.position = new Vector3(0, 0, 0);
+            rb.velocity = Vector2.zero;
+            direction = -1;
+            Invoke("StartBall", 1);
+            //rightScore++;
+            //rightText.text = rightScore.ToString();
+        }
+    }
 }
